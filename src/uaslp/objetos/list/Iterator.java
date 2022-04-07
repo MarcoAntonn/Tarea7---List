fb9989c452340b19459d85c97a2dbfd4b134f5ed
MarcoAntonn <maanloarr@gmail.com>
2022-04-07 18:54:30 -0500

    Interface

--- src/uaslp/objetos/list/Iterator.java
+++
@@ -1,7 +0,0 @@
-package uaslp.objetos.list;
-
-public interface Iterator <T>{ //T es creo el tipo de dato, cada T en cada clase es diferente, no tiene que ver uno con otro
-    //Los constructores no se ponen en las interfaces
-    boolean hasNext();
-    T next();
-}
--- src/uaslp/objetos/list/exception/NotNullValuesAllowedException.java
+++
@@ -1,7 +0,0 @@
-package uaslp.objetos.list.exception;
-
-public class NotNullValuesAllowedException extends Exception{
-    public NotNullValuesAllowedException(){
-        super("Null values are not allowed");
-    }
-}
